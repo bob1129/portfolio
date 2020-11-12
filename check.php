3cdccd781b731d18462d04379070da3cb1cd358f
bob1129 <abarabob@i.softbank.jp>
2020-11-12 14:02:45 +0900

    commit

---
+++ check.php
@@ -0,0 +1,108 @@
+<?php
+session_start();
+$_SESSION = [];
+
+require_once('is_mail.php');
+
+$name = htmlspecialchars($_POST['name']);
+$email = htmlspecialchars($_POST['email']);
+$subject = htmlspecialchars($_POST['subject']);
+$message = htmlspecialchars($_POST['message']);
+
+$_SESSION['name'] = $name;
+$_SESSION['email'] = $email;
+$_SESSION['subject'] = $subject;
+$_SESSION['message'] = $message;
+
+//入力チェックエラーとエラーメッセージ
+$flg = 0;
+
+if(empty($name)){
+    $_SESSION['name_error'] = '<span class="attention">名前を入力してください</span>';
+    $flg = 1;
+}
+
+if(empty($email)){
+    $_SESSION['email_error'] = '<span class="attention">メールアドレスを入力してください</span>';
+    $flg = 1;
+}else if(!is_mail($email)){
+    $_SESSION['email_error'] = '<span class="attention">メールアドレスを確認してください</span>';
+    $flg = 1;
+}
+
+// if($flg){
+//     header('Location: '.$_SERVER['HTTP_REFERER']);
+// }
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <!-- Google Tag Manager -->
+  <script>(function (w, d, s, l, i) {
+      w[l] = w[l] || []; w[l].push({
+        'gtm.start':
+          new Date().getTime(), event: 'gtm.js'
+      }); var f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''; j.async = true; j.src =
+          'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-TKNDDPB');</script>
+  <!-- End Google Tag Manager -->
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>portfolio</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.5.8/dist/css/uikit.min.css">
+  <link rel="stylesheet" href="common/css/reset.css">
+  <link rel="stylesheet" href="common/css/style.css">
+  <!-- <script src="https://kit.fontawesome.com/dbae26921f.js"></script> -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/uikit@3.5.8/dist/js/uikit.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/uikit@3.5.8/dist/js/uikit-icons.min.js"></script>
+  <script src="common/js/main.js"></script>
+</head>
+
+<body>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TKNDDPB" height="0" width="0"
+      style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+
+  <!-- CONTACT -->
+  <section id="contact">
+    <div class="inner">
+      <h2 class="title">CONTACT FORM</h2>
+
+      <dl>
+         <dt>Your Name</dt>
+         <dd><?= $name; ?></dd>
+      </dl>
+      <dl>
+         <dt>Your Email</dt>
+         <dd><?= $email; ?></dd>
+      </dl>
+      <dl>
+        <dt>Subject</dt>
+        <dd><?= $subject; ?></dd>
+      </dl>
+      <dl>
+        <dt>Your Message</dt>
+        <dd><?= $message; ?></dd>
+      </dl>
+
+      <p class="uk-button uk-button-primary"><a href="index.html">戻る</a></p>
+      <p class="uk-button uk-button-primary"><a href="send.php">この内容で送信</a></p>
+
+    </div>
+  </section>
+
+  <!-- FOOTER -->
+  <footer>
+    <div class="inner">
+      <p><small>Copyright&COPY;Toyokazu Mori. All Rights Reserved.</small></p>
+    </div>
+  </footer>
+</body>
+
+</html>
---
+++ send.php
@@ -0,0 +1,74 @@
+<?php
+session_start();
+
+$name = $_SESSION['name'];
+$email = $_SESSION['email'];
+$subject = $_SESSION['subject'];
+$message = $_SESSION['message'];
+
+$_SESSION = [];
+
+$agent = $_SERVER['HTTP_USER_AGENT'];
+
+//自動返信メール
+$email_title = 'お問い合わせありがとうございます';
+$mail_from = 'From:info@toyokazu-mori.com';
+$email_body = <<<mail
+{$name}様
+
+※こちらのメールは、自動返信メールです。
+
+この度は、お問い合わせいただき、ありがとうございます。
+以下の内容で承りました。
+
+お名前
+{$name}様
+
+メールアドレス
+{$email}
+
+題名
+{$subject}
+
+メッセージ
+{$message}
+
+mail;
+
+mb_internal_encoding('UTF-8');
+mb_language('japanese');
+mb_send_mail($email,$email_title,$mail_body,$mail_from);
+
+//管理人にメール
+$kanri_email = 'abarabob@i.softbank.jp';
+$kanri_title = 'ホームページよりお問い合わせがありました';
+
+$kanri_body = <<<kanri
+ホームページよりお問い合わせがありました
+
+お名前
+{$name}様
+
+メールアドレス
+{$email}
+
+題名
+{$subject}
+
+メッセージ
+{$message}
+
+============
+<ユーザーエージェント>
+$agent
+
+kanri;
+
+
+mb_internal_encoding('UTF-8');
+mb_language('japanese');
+mb_send_mail($kanri_email,$kanri_title,$kanri_body,$mail_from);
+
+header('Location:https://toyokazu-mori.com/portfolio/thanks.html')
+
+?>
